8919b61cb74b11eb8789c60e14d1b7819c854264
lleplat <leo.leplat@centrale.centralelille.fr>
2020-06-08 17:26:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coursepro/GenericActivity.kt
@@ -0,0 +1,34 @@
+package com.example.coursepro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+open class GenericActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    /*
+    Menu
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when(id) {
+            R.id.menuPreferences -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
---
+++ app/src/main/java/com/example/coursepro/SettingsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.coursepro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coursepro.fragments.SettingsFragment
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings_container, SettingsFragment())
+            .commit()
+    }
+}
