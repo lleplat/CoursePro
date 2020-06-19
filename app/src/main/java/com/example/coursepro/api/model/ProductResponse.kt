77b082d7c89e53cdaf7bac6043832260bc0e6659
lleplat <leo.leplat@centrale.centralelille.fr>
2020-06-19 20:23:21 +0200

    Improved barcode detection

--- app/src/main/java/com/example/coursepro/api/model/ProductResponse.kt
+++ app/src/main/java/com/example/coursepro/api/model/ProductResponse.kt
@@ -1,4 +1,17 @@
 package com.example.coursepro.api.model
 
-class ProductResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class APIResponse (
+
+    @SerializedName("products")
+    val product : List<ProductResponse>
+
+)
+
+data class ProductResponse (
+
+    @SerializedName("product_name")
+    val product_name : String
+
+)
