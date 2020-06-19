22d1d8a6dd442ed3f45345e0fcb132d7f59339c4
lleplat <leo.leplat@centrale.centralelille.fr>
2020-06-19 15:54:24 +0200

    Added barcode & product API

---
+++ app/src/main/java/com/example/coursepro/BarcodeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coursepro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BarcodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barcode)
+    }
+}
